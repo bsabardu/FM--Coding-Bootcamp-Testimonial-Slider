e92e3d6616ca4b3805fd2930ad802ab664448d2a
Benjamin Sabardu <bsabardu@gmail.com>
2020-08-21 00:31:30 +0200

    End front integration for mobile

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-$(document).ready(function() {
+/* $(document).ready(function() {
     $('.carrousel').slick({
         arrows: true,
         prevArrow: '.fa-angle-left',
         nextArrow: '.fa-angle-right'
 
     });
-});
+}); */
