909f62c72ed310060ca4b350d9b943c23349f123
Benjamin Sabardu <bsabardu@gmail.com>
2020-08-24 15:06:49 +0200

    init jquery

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
-/* $(document).ready(function() {
-    $('.carrousel').slick({
-        arrows: true,
-        prevArrow: '.fa-angle-left',
-        nextArrow: '.fa-angle-right'
+const Tanya = {
+    id: "1",
+    name: "Tanya Sinclair",
+    job: "UX Designer",
+    img_url: "/img/image-tanya.jpg",
+    quote: " I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. "
+}
 
-    });
-}); */
+const John = {
+    id: "2",
+    name: "John Tarkpor",
+    job: "Junior Front-end Developer",
+    img_url: "/img/image-john.jpg",
+    quote: " If you want to lay the best foundation possible I'd recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a profesionnal developer. "
+}
+
+
+function loadStudent(student) {
+    $("img").attr("src", student.img_url);
+    $(".name").text(student.name);
+    $(".job").text(student.job);
+    $(".quote").text(student.quote);
+    /*     document.getElementsByTagName("img").src = student.img_url;
+        document.getElementsByClassName("name")[0].innerText = student.name;
+        document.getElementsByClassName("job")[0].innerText = student.job;
+        document.getElementsByClassName("quote")[0].innerText = student.job; */
+}
+
+$(document).ready(function() {
+    loadStudent(John)
+})
